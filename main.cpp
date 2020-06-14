13413fc78afe3b1483905c13c0bd9883d23d1c41
margaXD <58165730+margaXD@users.noreply.github.com>
2020-06-14 23:02:00 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -7,22 +7,22 @@ int main()
         if(it->first.size() == 0)
         throw std::runtime_error("Failas tuscias");
         bool tus = true;
-        for(const auto& x : Tekstas)
+        for(const auto& skaic : Tekstas)
         {
             tus = false;
-            std::cout<<x.first;
-            for(int i=0; i<20-x.first.size(); i++)
+            std::cout<<skaic.first;
+            for(int i=0; i<20-skaic.first.size(); i++)
             {
                 std::cout<<" ";
             }
-            std::cout<<"  pasikartoja "<<x.second.size() << "kartus eilutese";
-            for(const auto&a : x.second)
+            std::cout<<"  pasikartoja "<<skaic.second.size() << "kartus eilutese";
+            for(const auto&a : skaic.second)
             {
                 std::cout<<a<<" ";
             }
-            std::cout<<std::endl;
+            std::cout<<"\n";
         }
-    if(tus)
+    if(tus == true)
         {
         std::cout<<"Zodziu nera"<<std::endl;
     }
